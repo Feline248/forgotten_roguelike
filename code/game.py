37732757097a92d3cb6b54e261413d86a5e570a1
Rachel <rad044@latech.edu>
2024-02-24 15:45:16 -0600

    added game class and main.py file

---
+++ code/game.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Game():
+
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((500,500))
+
+    def get_input(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def respond(self, input):
+        pass
+
+    def play(self):
+        pass
+
+

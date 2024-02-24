aab2bc6b0813f3958e0e21376b68c122439b4d65
Rachel <rad044@latech.edu>
2024-02-24 16:07:45 -0600

    added colors.py

---
+++ graphics/colors.py
@@ -0,0 +1,43 @@
+#defines colors to be used in the game
+
+#basic colors
+BLACK = "#000000"
+WHITE = "#FFFFFF"
+BLUE = "#0000FF"
+GREEN = "#00FF00"
+RED = "#FF0000"
+CYAN = "#00FFFF"
+MAGENTA = "#FF00FF"
+YELLOW = "#FFFF00"
+
+#load screen palette
+SHARP_MINT ="#79FFC0"
+TURQUOISE = "#00BCBC"
+MIST = "#C0F6EC"
+STORMY_GRAY = "#414A64"
+HOT_PINK = "#FF008F"
+ROSE_PETAL = "#FCDBED"
+STRAWBERRY_CANDY = "#FF77AA"
+SPRING_LEAVES = "#E3FDD7"
+
+#tutorial palette
+
+
+#greenhouse palette
+ORCHID = "#EBD7FD"
+
+#ruins palette
+
+
+#lab palette
+
+
+#stacks palette
+
+
+#dungeon palette
+MAUVE = "#9D74A2"
+DUSK = "#7B81B0"
+NAVY_BLUE = "#010842"
+SHADOWS = "#3D3C4F"
+ICE_BLUE = "#D9EEF8"
